527a872640334ae5abaf1f9ee8348b0ef7cdfff4
shleykin <ishleykin@gmail.com>
2019-01-29 12:59:31 +0300

    Вроде все работает )

---
+++ l1_Shleykin.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+//1lesson_ShleykinIvan
+
+//1. Решить квадратное уравнение.
+//ax²+bx+c=0, x=(-b±√D)/2a, D=b²-4ac
+
+let a = 5.0
+let b = 8.0
+let c = 12.0
+
+var x: Double
+var d: Double
+
+//вычислим Дискриминант
+
+d = pow(b,2) - (4*a*c)
+
+print(d)
---
+++ l1_Shleykin/l1_Shleykin/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  l1_Shleykin
+//
+//  Created by Ivan Shleykin on 29/01/2019.
+//  Copyright © 2019 Ivan Shleykin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
