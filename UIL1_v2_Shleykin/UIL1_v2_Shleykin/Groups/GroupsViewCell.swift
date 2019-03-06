1ea187c7465bd6ea62bebb1d4218b49989321d45
shleykin <ishleykin@gmail.com>
2019-03-06 12:45:43 +0300

    add groups

---
+++ UIL1_v2_Shleykin/UIL1_v2_Shleykin/Groups/GroupsViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  GroupsViewCell.swift
+//  UIL1_v2_Shleykin
+//
+//  Created by Ivan Shleykin on 06/03/2019.
+//  Copyright © 2019 Ivan Shleykin. All rights reserved.
+//
+
+import UIKit
+
+class GroupsViewCell: UITableViewCell {
+
+   
+    @IBOutlet weak var groupName: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
