24b7ed673461f5b5dceb757a46c1e9574dec5e96
shleykin <ishleykin@gmail.com>
2019-03-06 10:54:50 +0300

    add friends array and 2 friends controllers

---
+++ UIL1_v2_Shleykin/UIL1_v2_Shleykin/Friends/FriendsViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendsViewCell.swift
+//  UIL1_v2_Shleykin
+//
+//  Created by Ivan Shleykin on 06/03/2019.
+//  Copyright © 2019 Ivan Shleykin. All rights reserved.
+//
+
+import UIKit
+
+class FriendsViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
