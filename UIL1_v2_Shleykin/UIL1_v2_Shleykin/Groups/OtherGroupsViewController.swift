a5f7fd85d4c6c6ff11d3b17e7db164b31e388c6f
shleykin <ishleykin@gmail.com>
2019-03-06 14:22:40 +0300

    try to add new group to table view , BAD

---
+++ UIL1_v2_Shleykin/UIL1_v2_Shleykin/Groups/OtherGroupsViewController.swift
@@ -0,0 +1,88 @@
+//
+//  OtherGroupsViewController.swift
+//  UIL1_v2_Shleykin
+//
+//  Created by Ivan Shleykin on 06/03/2019.
+//  Copyright © 2019 Ivan Shleykin. All rights reserved.
+//
+
+import UIKit
+
+class OtherGroupsViewController: UITableViewController {
+    
+    var otherGroups = ["Металлисты","Рэперы","Фолк"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+ 
+
+   // override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+   //     return 1
+   //}
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return otherGroups.count
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "OtherGroupCell", for: indexPath)
+        
+        cell.textLabel?.text = otherGroups[indexPath.row]
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
