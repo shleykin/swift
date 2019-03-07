649159e4d980d5bdd4c7bf97699a2133c7352d4b
shleykin <ishleykin@gmail.com>
2019-03-07 13:30:58 +0300

    add Avatars cell and View controller

---
+++ UIL1_v2_Shleykin/UIL1_v2_Shleykin/Friends/AvatarsViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  AvatarsViewCell.swift
+//  UIL1_v2_Shleykin
+//
+//  Created by Ivan Shleykin on 07/03/2019.
+//  Copyright © 2019 Ivan Shleykin. All rights reserved.
+//
+
+import UIKit
+
+class AvatarsViewCell: UICollectionViewCell {
+    
+}
