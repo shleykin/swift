4f7dddd20673d6e5f3b2ade888ebd29eff1ad713
shleykin <ishleykin@gmail.com>
2019-03-13 10:36:33 +0300

    test

--- UIL1_v2_Shleykin/UIL1_v2_Shleykin/Friends/FriendsViewCell.swift
+++ UIL1_v2_Shleykin/UIL1_v2_Shleykin/Friends/FriendsViewCell.swift
@@ -18,31 +18,31 @@ class FriendsViewCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
         
-        //friendImage.layer.cornerRadius = friendImage.frame.height / 2
-        //friendImage.clipsToBounds = true
+        friendImage.layer.cornerRadius = friendImage.frame.height / 2
+        friendImage.clipsToBounds = true
         //friendImage.layer.shadowRadius = 4.0
         //friendImage.layer.shadowOpacity = 1
         //friendImage.layer.shadowOffset = CGSize.zero
         //friendImage.layer.shadowPath = UIBezierPath(roundedRect: friendImage.bounds , cornerRadius: friendImage.frame.height / 2 ).cgPath
         //friendImage.layer.shouldRasterize = true
+            
+        //let imageCotainer = UIView(frame: CGRect(x: 0, y: 0, width: friendImage.frame.width, height: friendImage.frame.height))
+        //imageCotainer.clipsToBounds = false
+        //imageCotainer.layer.shadowColor = UIColor.black.cgColor
+        //mageCotainer.layer.shadowOpacity = 1
+        //imageCotainer.layer.shadowOffset = CGSize(width: 0, height: 1)
+        //imageCotainer.layer.shadowRadius = 4
         
-        let imageCotainer = UIView(frame: CGRect(x: 0, y: 0, width: friendImage.frame.width, height: friendImage.frame.height))
-        imageCotainer.clipsToBounds = false
-        imageCotainer.layer.shadowColor = UIColor.black.cgColor
-        imageCotainer.layer.shadowOpacity = 0.2
-        imageCotainer.layer.shadowOffset = CGSize(width: 0, height: 1)
-        imageCotainer.layer.shadowRadius = 2
         
+        //var imageView1 = UIImageView(frame: imageCotainer.bounds)
+        //imageView1 = friendImage
+        //imageView1.clipsToBounds = true
+        //let cornerRadius = imageView1.frame.height / 2
+        //imageView1.layer.cornerRadius = cornerRadius
         
-        var imageView1 = UIImageView(frame: imageCotainer.bounds)
-        imageView1 = friendImage
-        imageView1.clipsToBounds = true
-        let cornerRadius = imageView1.frame.height / 2
-        imageView1.layer.cornerRadius = cornerRadius
-        
-        imageCotainer.layer.shadowPath = UIBezierPath(roundedRect: imageCotainer.bounds, cornerRadius: cornerRadius).cgPath
+        //imageCotainer.layer.shadowPath = UIBezierPath(roundedRect: imageCotainer.bounds, cornerRadius: cornerRadius).cgPath
         // Add image into container
-        imageCotainer.addSubview(imageView1)
+        //imageCotainer.addSubview(imageView1)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -52,25 +52,45 @@ class FriendsViewCell: UITableViewCell {
     }
 
 }
-/*let imageSize: CGFloat = 64.0
 
-// Create a container which has a shadow
-let imageCotainer = UIView(frame: CGRect(x: 0, y: 0, width: imageSize, height: imageSize))
-imageCotainer.clipsToBounds = false
-imageCotainer.layer.shadowColor = UIColor.black.cgColor
-imageCotainer.layer.shadowOpacity = 0.2
-imageCotainer.layer.shadowOffset = CGSize(width: 0, height: 1)
-imageCotainer.layer.shadowRadius = 2
-
-// Create an image view that will be inserted into the container view
-let imageView = UIImageView(frame: imageCotainer.bounds)
-imageView.image = yourImage
-imageView.clipsToBounds = true
-let cornerRadius = imageView.frame.height / 2
-imageView.layer.cornerRadius = cornerRadius
-
-// Draw a shadow
-imageCotainer.layer.shadowPath = UIBezierPath(roundedRect: imageCotainer.bounds, cornerRadius: cornerRadius).cgPath
-// Add image into container
-imageCotainer.addSubview(imageView)
-*/
+/*
+ class IconAvatar: UIImageView {
+ 
+ let imageSize: (height: CGFloat, width: CGFloat) = (70.0, 70.0)
+ let cornerRadius: CGFloat = 35.0
+ @IBInspectable var shadow: Bool = true
+ @IBInspectable var shadowCollor: UIColor = .black
+ @IBInspectable var shadowOpacity: Float = 0.7
+ @IBInspectable var shadowOffset: CGSize = CGSize(width: 5.0, height: 5.0)
+ @IBInspectable var shadowRadius: CGFloat = 5.0
+ 
+ func createIconAvatar(image name: String) {
+ 
+ let imageContainer = UIView(frame: CGRect(x: 0, y: 0, width: imageSize.width, height: imageSize.height))
+ 
+ 
+ if shadow {
+ imageContainer.clipsToBounds = false
+ imageContainer.layer.shadowColor = shadowCollor.cgColor
+ imageContainer.layer.shadowOpacity = shadowOpacity
+ imageContainer.layer.shadowOffset = shadowOffset
+ imageContainer.layer.shadowRadius = shadowRadius
+ imageContainer.layer.shadowPath = UIBezierPath(roundedRect: imageContainer.bounds, cornerRadius: cornerRadius).cgPath
+ }
+ 
+ let imageView = UIImageView(frame: imageContainer.bounds)
+ 
+ imageView.clipsToBounds = true
+ imageView.layer.cornerRadius = cornerRadius
+ imageView.layer.borderColor = UIColor.black.cgColor
+ imageView.layer.borderWidth = 1.0
+ imageView.image = UIImage(named: name)
+ 
+ self.addSubview(imageContainer)
+ imageContainer.addSubview(imageView)
+ 
+ }
+ 
+ 
+ }
+ */
--- UIL1_v2_Shleykin/UIL1_v2_Shleykin/Friends/IconAvatar.swift
+++ UIL1_v2_Shleykin/UIL1_v2_Shleykin/Friends/IconAvatar.swift
@@ -2,7 +2,7 @@
 //  IconAvatar.swift
 //  UIL1_v2_Shleykin
 //
-//  Created by Ivan Shleykin on 12/03/2019.
+//  Created by Ivan Shleykin on 13/03/2019.
 //  Copyright © 2019 Ivan Shleykin. All rights reserved.
 //
 
@@ -10,15 +10,38 @@ import UIKit
 
 class IconAvatar: UIImageView {
 
+    let imageSize: (height: CGFloat, width: CGFloat) = (43.0, 43.0)
+    let cornerRadius: CGFloat = 21.5
+    @IBInspectable var shadow: Bool = true
+    @IBInspectable var shadowCollor: UIColor = .black
+    @IBInspectable var shadowOpacity: Float = 0.7
+    @IBInspectable var shadowOffset: CGSize = CGSize(width: 5.0, height: 5.0)
+    @IBInspectable var shadowRadius: CGFloat = 5.0
     
-    let cornerRadius: CGFloat = 35.0
-    
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    func createIconAvatar(image name: String) {
+        
+        let imageContainer = UIView(frame: CGRect(x: 0, y: 0, width: imageSize.width, height: imageSize.height))
+        
+        
+        if shadow {
+            imageContainer.clipsToBounds = false
+            imageContainer.layer.shadowColor = shadowCollor.cgColor
+            imageContainer.layer.shadowOpacity = shadowOpacity
+            imageContainer.layer.shadowOffset = shadowOffset
+            imageContainer.layer.shadowRadius = shadowRadius
+            imageContainer.layer.shadowPath = UIBezierPath(roundedRect: imageContainer.bounds, cornerRadius: cornerRadius).cgPath
+        }
+        
+        let imageView = UIImageView(frame: imageContainer.bounds)
+        
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = cornerRadius
+        imageView.layer.borderColor = UIColor.black.cgColor
+        imageView.layer.borderWidth = 1.0
+        imageView.image = UIImage(named: name)
+        
+        self.addSubview(imageContainer)
+        imageContainer.addSubview(imageView)
 
+}
 }
